95b050bbf6d05d6ed7cbb09992b7cf56e433bb9d
Mazz <117194710+oxMAZZxo@users.noreply.github.com>
2024-11-21 23:42:41 +0000

    Update Session.cs
    Changed Code Structure
    Added colours for guessing right or wrong
    Added Option for user choosing to shuffle

--- Higher-Lower/Session.cs
+++ Higher-Lower/Session.cs
@@ -1,8 +1,4 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Diagnostics;
-using System.Drawing;
-using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using Microsoft.VisualBasic;
@@ -11,8 +7,10 @@ namespace Higher_Lower
 {
 public class Session
 {
-    const int cardXWidth = 11;
-    const int cardYWidth = 11;
+    const int cardWidth = 13;
+    const int cardHeight = 11;
+    const int cardXPadding = 2;
+    const int cardYPadding = 2;
     const int scorePosX = 0;
     const int scorePosY = 0;
     const int resetPosX = 30;
@@ -21,6 +19,7 @@ public class Session
     const int cardDrawPosY = 2;
     const int questionPosX = 0;
     const int questionPosY = 15;
+    const int delayUntilNextCard = 5000;
     private Deck myDeck;
     private string? userInput;
     private bool exitGame;
@@ -32,8 +31,6 @@ public class Session
     /// </summary>
     public Session()
     {
-        Console.WriteLine("Welcome to Higher/Lower");
-        Console.WriteLine("Creating deck....");
         Console.Write("Create Jokers? Enter Y/N or yes/no: ");
         userInput = Console.ReadLine();
         userInput = userInput?.ToUpper();
@@ -42,7 +39,18 @@ public class Session
         {
             createJokers = true;
         }
-        myDeck = new Deck(createJokers, false);
+
+        Console.Write("Shuffle Deck? Enter Y/N or yes/no: ");
+        userInput = Console.ReadLine();
+        userInput = userInput?.ToUpper();
+        
+        bool shuffleDeck = false;
+        if(userInput == "Y" || userInput == "YES")
+        {
+            shuffleDeck = true;
+        }
+
+        myDeck = new Deck(createJokers, shuffleDeck);
         noOfResets = 0;
         score = 0;
         exitGame = false;
@@ -53,13 +61,25 @@ public class Session
     /// </summary>
     public void StartGame()
     {
+        bool firstRun = true;
+        Card hiddenCard = myDeck.GetRandomCard(); // this is done to avoid the compiler error (Use of unassigned local variable) on line 78
+        Card visibleCard;
         while(!exitGame)
         {
-            Console.Clear();
+            userInput = null;
+            if(firstRun)
+            {
+                firstRun = false;
+                visibleCard = myDeck.GetRandomCard();
+            }else
+            {
+                visibleCard = hiddenCard;
+            }
+            hiddenCard = myDeck.GetRandomCard();
+            DisplayHUD();
 
-            Card visibleCard = myDeck.GetRandomCard();
-            Card hiddenCard = myDeck.GetRandomCard();
-            DisplayUI(visibleCard,hiddenCard);
+            DrawCard(visibleCard,cardDrawPosX,cardDrawPosY);
+            DrawCardFaceDown(cardDrawPosX + cardWidth + 2, cardDrawPosY);
 
             Console.ForegroundColor = ConsoleColor.Gray;
             Console.BackgroundColor = ConsoleColor.Black;
@@ -67,71 +87,75 @@ public class Session
             Console.Write("Is the hidden card higher, lower, or equal? H/L/E: ");
             userInput = Console.ReadLine()?.ToUpper();
             
-            bool isHiddenCardHigher = false;
-            bool equal = false;
+            string message;
+            bool correct = CalculateResponse(visibleCard,hiddenCard,out message);
 
-            if(visibleCard.GetCardValue() == hiddenCard.GetCardValue())
-            {
-                equal = true;
-            }
-
-            if(!equal)
-            {
-                if(visibleCard.GetCardValue() < hiddenCard.GetCardValue()) {isHiddenCardHigher = true;}
-            }
-
-            string message = "You are wrong!";
-            bool correct = false;
-            switch(userInput)
-            {
-                case "E":
-                    if(equal) {
-                        message = "You are corect! They are equal in value.";
-                        correct = true;
-                    }
-                break;
-                case "H":
-                    if(isHiddenCardHigher)
-                    {
-                        message = "You are correct! The hidden card has a higher value!";
-                        correct = true;
-                    }
-                break;
-                case "L":
-                    if(!isHiddenCardHigher)
-                    {
-                        message = "You are correct! The hidden card has a lower value";
-                        correct = true;
-                    }
-                break;
-            }
-
-            DisplayResult(hiddenCard,message);
             if(correct) {score ++;} else{noOfResets ++;}
-            Console.ReadLine();
+            DisplayHUD();
+            DrawCard(visibleCard,cardDrawPosX, cardDrawPosY);
+            DrawCard(hiddenCard,cardDrawPosX + cardWidth + 2, cardDrawPosY);
+            Console.BackgroundColor = ConsoleColor.Black;
+            Console.ForegroundColor = ConsoleColor.Gray;
+            Console.SetCursorPosition(questionPosX,questionPosY);
+            if(correct) {Console.ForegroundColor = ConsoleColor.Green;} else{Console.ForegroundColor = ConsoleColor.Red;}
+            Console.Write(message);
+            Thread.Sleep(delayUntilNextCard);
         }
     }
 
-    void DisplayUI(Card visibleCard,Card hiddenCard)
+    void DisplayHUD()
     {
+        Console.Clear();
         Console.BackgroundColor = ConsoleColor.Black;
         Console.ForegroundColor = ConsoleColor.Gray;
         Console.SetCursorPosition(scorePosX,scorePosY);
         Console.Write($"Score: {score}");
         Console.SetCursorPosition(resetPosX,resetPosY);
         Console.Write($"Resets: {noOfResets}");
-            
-        DrawCard(visibleCard,cardDrawPosX,cardDrawPosY);
-        DrawCard(hiddenCard,cardDrawPosX + cardXWidth + 2, cardDrawPosY);
     }
 
-    void DisplayResult(Card hiddenCard, string message)
+    bool CalculateResponse(Card visibleCard, Card hiddenCard, out string message)
     {
-        DrawCard(hiddenCard,cardDrawPosX + cardXWidth + 2, cardDrawPosY);
-        Console.ForegroundColor = ConsoleColor.Gray;
-        Console.BackgroundColor = ConsoleColor.Black;
-        Console.SetCursorPosition(questionPosX,questionPosY);
-        Console.Write(message);
+        bool isHiddenCardHigher = false;
+        bool equal = false;
+
+        if(visibleCard.GetCardValue() == hiddenCard.GetCardValue())
+        {
+            equal = true;
+        }
+
+        if(!equal)
+        {
+            if(visibleCard.GetCardValue() < hiddenCard.GetCardValue()) {isHiddenCardHigher = true;}
+        }
+
+        message = "You are wrong!";
+        bool correct = false;
+        switch(userInput)
+        {
+            case "E":
+                if(equal) {
+                    message = "You are corect! They are equal in value.";
+                    correct = true;
+                }
+            break;
+            case "H":
+                if(isHiddenCardHigher && !equal)
+                {
+                    message = "You are correct! The hidden card has a higher value!";
+                    correct = true;
+                }
+            break;
+            case "L":
+                if(!isHiddenCardHigher && !equal)
+                {
+                    message = "You are correct! The hidden card has a lower value";
+                    correct = true;
+                }
+            break;
+        }
+
+        return correct;
     }
 
     /// <summary>
@@ -145,9 +169,9 @@ public class Session
         int charCodeToPrint;
         Console.BackgroundColor = ConsoleColor.White;
         Console.ForegroundColor = ConsoleColor.Black;
-        for (int y = 0; y < cardYWidth + 1; y++)
+        for (int y = 0; y < cardHeight + 1; y++)
         {
-            for (int x = 0; x < cardXWidth + 1; x++)
+            for (int x = 0; x < cardWidth + 1; x++)
             {
                 charCodeToPrint = 32;
                 Console.SetCursorPosition(x + startX, y + startY);
@@ -155,23 +179,23 @@ public class Session
                 {
                     charCodeToPrint = 9555; // Top left corner
                 }
-                else if (y == 0 && x == cardXWidth)
+                else if (y == 0 && x == cardWidth)
                 {
                     charCodeToPrint = 9558; // Top right corner
                 }
-                else if (y == cardYWidth && x == 0)
+                else if (y == cardHeight && x == 0)
                 {
                     charCodeToPrint = 9561; // Bottom left corner
                 }
-                else if (y == cardYWidth && x == cardXWidth)
+                else if (y == cardHeight && x == cardWidth)
                 {
                     charCodeToPrint = 9564; // Bottom right corner
                 }
-                else if (y == 0 || y == cardYWidth)
+                else if (y == 0 || y == cardHeight)
                 {
                     charCodeToPrint = 9552; // Top or bottom row
                 }
-                else if (x == 0 || x == cardXWidth)
+                else if (x == 0 || x == cardWidth)
                 {
                     charCodeToPrint = 9553; // Left or right column
                 }
@@ -181,16 +205,27 @@ public class Session
             Console.WriteLine();
         }
 
+        if(card.value == "J" || card.value == "Q" || card.value == "K" || card.value == "Joker")
+        {
+            DrawPictureCard(startX,startY);
+        }
+
         Console.SetCursorPosition(startX + 2, startY + 1);
         Console.ForegroundColor = card.suit.color;
+        Console.BackgroundColor = ConsoleColor.White;
         Console.Write(card.value);
         Console.SetCursorPosition(startX + 2, startY + 2);
         Console.Write(card.suit.code);
-        Console.SetCursorPosition(startX + cardXWidth - 2, startY + cardYWidth - 2);
+        Console.SetCursorPosition(startX + cardWidth - 2, startY + cardHeight - 2);
         Console.Write(card.suit.code);
-        Console.SetCursorPosition(startX + cardXWidth - 2, startY + cardYWidth - 1);
-        Console.Write(card.value);
-
+        if(card.value == "Joker")
+        {
+            Console.SetCursorPosition(startX + cardWidth - 5, startY + cardHeight - 1);
+        }else
+        {
+            Console.SetCursorPosition(startX + cardWidth - 2, startY + cardHeight - 1);
+        }
+            Console.Write(card.value);
     }
 
     /// <summary>
@@ -203,9 +238,9 @@ public class Session
         int charCodeToPrint;
         Console.BackgroundColor = ConsoleColor.White;
         Console.ForegroundColor = ConsoleColor.Black;
-        for (int y = 0; y < cardYWidth + 1; y++)
+        for (int y = 0; y < cardHeight + 1; y++)
         {
-            for (int x = 0; x < cardXWidth + 1; x++)
+            for (int x = 0; x < cardWidth + 1; x++)
             {
                 charCodeToPrint = 32;
                 Console.SetCursorPosition(x + startX, y + startY);
@@ -213,23 +248,23 @@ public class Session
                 {
                     charCodeToPrint = 9555; // Top left corner
                 }
-                else if (y == 0 && x == cardXWidth)
+                else if (y == 0 && x == cardWidth)
                 {
                     charCodeToPrint = 9558; // Top right corner
                 }
-                else if (y == cardYWidth && x == 0)
+                else if (y == cardHeight && x == 0)
                 {
                     charCodeToPrint = 9561; // Bottom left corner
                 }
-                else if (y == cardYWidth && x == cardXWidth)
+                else if (y == cardHeight && x == cardWidth)
                 {
                     charCodeToPrint = 9564; // Bottom right corner
                 }
-                else if (y == 0 || y == cardYWidth)
+                else if (y == 0 || y == cardHeight)
                 {
                     charCodeToPrint = 9552; // Top or bottom row
                 }
-                else if (x == 0 || x == cardXWidth)
+                else if (x == 0 || x == cardWidth)
                 {
                     charCodeToPrint = 9553; // Left or right column
                 }
@@ -237,6 +272,20 @@ public class Session
                 Console.Write(Strings.ChrW(charCodeToPrint));
             }
             Console.WriteLine();
+        }
+    }
+
+    void DrawPictureCard(int startX, int startY)
+    {
+        Random rnd = new Random();
+        for(int y = cardYPadding; y < cardHeight + 1 - cardYPadding; y++)
+        {
+            for(int x = cardXPadding; x < cardWidth + 1 - cardXPadding; x++)
+            {
+                Console.BackgroundColor = (ConsoleColor)rnd.Next(15);
+                Console.SetCursorPosition(x + startX, y + startY);
+                Console.Write(" ");
+            }
         }
     }
 
@@ -249,9 +298,9 @@ public class Session
     {
         int nextCard = 0;
         int lastY = 0;
-        for (int y = 0; y < (cardYWidth + 2) * 4; y += cardYWidth + 2)
+        for (int y = 0; y < (cardHeight + 2) * 4; y += cardHeight + 2)
         {
-            for (int x = 0; x < (cardXWidth + 2) * 13; x += cardXWidth + 2)
+            for (int x = 0; x < (cardWidth + 2) * 13; x += cardWidth + 2)
             {
                 Thread.Sleep(50);
                 DrawCard(myDeck.GetCard(nextCard), x, y);
@@ -260,12 +309,12 @@ public class Session
             lastY = y; 
         }
 
-        for(int i = 0; i < (cardXWidth + 2) * 2; i += cardXWidth + 2)
+        for(int i = 0; i < (cardWidth + 2) * 2; i += cardWidth + 2)
         {
             DrawCard(myDeck.GetCard(myDeck.blackJokerIndex),i,lastY);
         }
 
     }
-
+    
 }
 }
